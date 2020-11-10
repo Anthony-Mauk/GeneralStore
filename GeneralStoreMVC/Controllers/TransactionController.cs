1c11ed5baed6a7465b2b1a2a4faaa32f38a17a54
Anthony-Mauk <anthonymauk.2020@gmail.com>
2020-11-10 14:47:39 -0500

    final commit

---
+++ GeneralStoreMVC/Controllers/TransactionController.cs
@@ -0,0 +1,124 @@
+﻿using GeneralStoreMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStoreMVC.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
+        // GET: Transaction
+
+        public ActionResult Index()
+        {
+            List<Transaction> transactionList = _db.Transactions.ToList();
+            List<Transaction> orderedList = transactionList.OrderBy(tran => tran.DateOfTransaction).ToList();
+            return View(orderedList);
+        }
+
+        // Get: Transaction
+        public ActionResult Create()
+        {
+            ViewBag.CustomerId = new SelectList(_db.Customers.ToList(), "CustomerId", "FullName");
+            ViewBag.ProductId = new SelectList(_db.Products.ToList(), "ProductId", "Name");
+            //ViewBag.ProductPrice = new SelectList(_db.Products.ToList(), "ProductId", "Price");
+            return View();
+        }
+        // POST: Transaction
+        [HttpPost]
+        public ActionResult Create(Transaction transaction)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.Transactions.Add(transaction);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            //ViewBag.CustomerId = new SelectList(_db.Customers.ToList(), "CustomerId", "FullName");
+            //ViewBag.ProductId = new SelectList(_db.Products.ToList(), "ProductId", "Name");
+            return View(transaction);
+        }
+
+        // GET : Delete
+        // Transaction/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Transaction transaction = _db.Transactions.Find(id);
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transaction);
+        }
+        // POST : Delete
+        // Transaction/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Transaction transaction = _db.Transactions.Find(id);
+            _db.Transactions.Remove(transaction);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET : Edit
+        // Transaction/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            ViewBag.CustomerId = new SelectList(_db.Customers.ToList(), "CustomerId", "FullName");
+            ViewBag.ProductId = new SelectList(_db.Products.ToList(), "ProductId", "Name");
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Transaction transaction = _db.Transactions.Find(id);
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transaction);
+        }
+        // POST : Edit// Transaction/Edit/{id}
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(transaction).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(transaction);
+        }
+        // GET : Details
+        // Transaction/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Transaction transaction = _db.Transactions.Find(id);
+
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(transaction);
+        }
+    }
+}
